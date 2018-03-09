8d5d36fc55839c0be44428a0becdfa33c9d82c41
Victor Gallet <victor.gallet@protonmail.ch>
2018-03-09 13:32:31 +0100

    Solution

--- src/test/java/fr/vga/kata/payment/PaymentServiceTest.java
+++ src/test/java/fr/vga/kata/payment/PaymentServiceTest.java
@@ -1,18 +1,56 @@
 package fr.vga.kata.payment;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class PaymentServiceTest {
+
+    private PaymentService paymentService;
+
+    @Mock
+    private PaymentRepository paymentRepository;
+
+    @Mock
+    private PaymentValidator paymentValidator;
+
+    private Payment payment;
+
+    @Before
+    public void setUp() throws Exception {
+        payment = new Payment();
+
+        paymentService = new PaymentService(paymentValidator, paymentRepository);
+    }
 
     @Test
     public void should_create_payment_when_is_valid() {
-        fail("TODO");
+        // GIVEN
+        when(paymentValidator.isValid(payment)).thenReturn(true);
+
+        // WHEN
+        paymentService.doPayment(payment);
+
+        // THEN
+        verify(paymentRepository, times(1)).create(payment);
     }
 
     @Test
     public void should_not_create_payment_when_is_invalid() {
-        fail("TODO");
+        // GIVEN
+        when(paymentValidator.isValid(payment)).thenReturn(true);
+
+        // WHEN
+        paymentService.doPayment(payment);
+
+        // THEN
+        verify(paymentRepository, times(1)).create(payment);
     }
 }
